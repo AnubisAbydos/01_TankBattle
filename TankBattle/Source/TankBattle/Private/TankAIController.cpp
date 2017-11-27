0b974698d4ebedb89d422b42c6ef672149b8f5f6
Lex <lex.anubis@outlook.com>
2017-11-27 15:58:16 -0700

    AI Controller Possesing and finding Player

---
+++ TankBattle/Source/TankBattle/Private/TankAIController.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankBattle.h"
+#include "TankAIController.h"
+
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI Can't find Player Tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI found Player: %s"), *(PlayerTank->GetName()));
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn)
+	{ 
+		return nullptr;
+	}
+	return Cast<ATank>(PlayerPawn);
+}
+
