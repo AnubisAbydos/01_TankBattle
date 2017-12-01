cfe47535fdfa5f4cb65051d41e0cbcf879362abd
Lex <lex.anubis@outlook.com>
2017-12-01 12:00:07 -0700

    Projectile Added

--- TankBattle/Source/TankBattle/Public/Tank.h
+++ TankBattle/Source/TankBattle/Public/Tank.h
@@ -37,7 +37,10 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void SetTurretReference(UTankTurret* TurretToSet);
 
+	UFUNCTION(BlueprintCallable, Category = Firing)
+	void Fire();
+
 	UPROPERTY(EditAnywhere, Category = Firing)
-	float LaunchSpeed = 100000;
+	float LaunchSpeed = 8000;
 	
 };
--- TankBattle/Source/TankBattle/Public/TankAimingComponent.h
+++ TankBattle/Source/TankBattle/Public/TankAimingComponent.h
@@ -27,9 +27,6 @@ protected:
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
 	void AimAt(FVector HitLocation, float LaunchSpeed);
 
 	void MoveBarrelTowards(FVector AimDirection);
