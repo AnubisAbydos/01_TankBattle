e7e6534a9c2cace3eb71e38d7ff3b18cb1214990
Lex <lex.anubis@outlook.com>
2017-12-19 12:10:06 -0700

    Tank Applies sideways force to avoid slippage and OnHit placed into track

--- TankBattle/Source/TankBattle/Private/TankTrack.cpp
+++ TankBattle/Source/TankBattle/Private/TankTrack.cpp
@@ -9,10 +9,26 @@ UTankTrack::UTankTrack()
 	PrimaryComponentTick.bCanEverTick = true;
 }
 
+void UTankTrack::BeginPlay()
+{
+	Super::BeginPlay();
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
 void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	//UE_LOG(LogTemp, Warning, TEXT("Track Ticking"));
+
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2;
+	TankRoot->AddForce(CorrectionForce);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent * HitComponent, AActor * OtherActor, UPrimitiveComponent * OtherComponent, FVector NormalImpulse, const FHitResult & Hit)
+{
+
 }
 
 void UTankTrack::SetThrottle(float Throttle)
--- TankBattle/Source/TankBattle/Public/TankTrack.h
+++ TankBattle/Source/TankBattle/Public/TankTrack.h
@@ -16,7 +16,12 @@ class TANKBATTLE_API UTankTrack : public UStaticMeshComponent
 private:
 	UTankTrack();
 
+	virtual void BeginPlay() override;
+
 	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 
 public:
 	UFUNCTION(BlueprintCallable, Category = Input)
